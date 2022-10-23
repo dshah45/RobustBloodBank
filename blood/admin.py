4f155a904aa129cc12c8bff717036bc6231a3a9c
dshah <dshah1359@gmail.com>
2022-10-23 11:50:49 +0530

    first commit

---
+++ blood/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Stock, BloodRequest
+admin.site.register(Stock)
+admin.site.register(BloodRequest)
---
+++ donor/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Donor, BloodDonate
+
+admin.site.register(Donor)
+admin.site.register(BloodDonate)
