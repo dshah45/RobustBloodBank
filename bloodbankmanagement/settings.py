653794b838805d630a6643f1fbc0d89f324b33e6
Dhruv Shah <59227940+dshah45@users.noreply.github.com>
2022-11-07 14:11:18 +0530

    updates

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -138,7 +138,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'robustblood@gmail.com' # this email will be used to send emails
-EMAIL_HOST_PASSWORD = 'mvncdwiksuyfgatp' # host email password required
+EMAIL_HOST_PASSWORD = '###' # host email password required
 # now sign in with your host gmail account in your browser
 # open following link and turn it ON
 # https://myaccount.google.com/lesssecureapps
